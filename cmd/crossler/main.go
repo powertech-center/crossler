697384095e58f17ac46ae016aacf16ffcb68ff47
Dmitry Mozulyov <d.mozulyov@powertech.center>
2026-03-10 17:20:25 +0300

    [ci skip]

--- cmd/crossler/main.go
+++ cmd/crossler/main.go
@@ -11,26 +11,32 @@ var Version = "0.0.0"
 func main() {
 	if len(os.Args) > 1 {
 		switch os.Args[1] {
-		case "version":
+		case "--version":
 			fmt.Println(Version)
-		case "help":
+			return
+		case "--help":
 			printHelp()
-		default:
-			fmt.Fprintf(os.Stderr, "Unknown command: %s\n", os.Args[1])
-			printHelp()
-			os.Exit(1)
+			return
 		}
+	}
+
+	if len(os.Args) == 1 {
+		fmt.Printf("Crossler %s\n", Version)
+		fmt.Println("Cross-platform packaging tool")
+		fmt.Println("Run 'crossler --help' for usage information.")
 		return
 	}
 
-	fmt.Printf("Crossler %s\n", Version)
-	fmt.Println("Cross-platform packaging tool")
-	fmt.Println("Run 'crossler help' for usage information.")
+	// First arg is the config file path
+	// TODO: implement config loading
+	fmt.Fprintf(os.Stderr, "Not implemented yet\n")
+	os.Exit(1)
 }
 
 func printHelp() {
 	fmt.Println("Usage:")
-	fmt.Println("  crossler              Show version and status")
-	fmt.Println("  crossler version      Show version number")
-	fmt.Println("  crossler help         Show this help message")
+	fmt.Println("  crossler                        Show version and status")
+	fmt.Println("  crossler --version              Show version number")
+	fmt.Println("  crossler --help                 Show this help message")
+	fmt.Println("  crossler <config> [key=value]   Build packages from config")
 }
